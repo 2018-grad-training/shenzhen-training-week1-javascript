8f1068033c5e12d100aeb4407fe7065c12e2de9b
Linesh <linesh.simpcity@gmail.com>
2018-08-25 16:00:00 +0800

    [Linesh] test: add more test cases

--- src/day-3/1-fibonacci/index.test.js
+++ src/day-3/1-fibonacci/index.test.js
@@ -15,3 +15,31 @@ it('should return 1 when calculate fibonacci(2)', () => {
 it('should return 2 when calculate fibonacci(3)', () => {
   expect(fibonacci(3)).toBe(2)
 })
+
+it('should return 3 when calculate fibonacci(4)', () => {
+  expect(fibonacci(4)).toBe(3)
+})
+
+it('should return 5 when calculate fibonacci(5)', () => {
+  expect(fibonacci(5)).toBe(5)
+})
+
+it('should return 8 when calculate fibonacci(6)', () => {
+  expect(fibonacci(6)).toBe(8)
+})
+
+it('should return 13 when calculate fibonacci(7)', () => {
+  expect(fibonacci(7)).toBe(13)
+})
+
+it('should return 21 when calculate fibonacci(8)', () => {
+  expect(fibonacci(8)).toBe(21)
+})
+
+it('should return 6765 when calculate fibonacci(20)', () => {
+  expect(fibonacci(20)).toBe(6765)
+})
+
+xit('should return 102334155 when calculate fibonacci(40)', () => {
+  expect(fibonacci(40)).toBe(102334155)
+})
