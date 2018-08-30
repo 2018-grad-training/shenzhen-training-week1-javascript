b6107b857136701f1b013c8e7b0b93aba81abb63
Linesh <linesh.simpcity@gmail.com>
2018-08-30 16:41:12 +0800

    [Linesh] refactor: inline getListMap

--- src/day-4-tdd-refactor/1-word-frequency/index.js
+++ src/day-4-tdd-refactor/1-word-frequency/index.js
@@ -3,32 +3,22 @@ const getResult = (words) => {
     return `${words} 1`
   }
 
-  const inputList = words
+  const result = words
     .split(' ')
     .filter((input) => input)
-    .map((input) => ({ value: input, count: 1 }))
+    .reduce(
+      (total, input) =>
+        Object.assign(total, {
+          [input]: total.hasOwnProperty(input) ? total[input] + 1 : 1,
+        }),
+      {}
+    )
 
-  // get the map for the next step of sizing the same word
-  const map = getListMap(inputList)
-
-  return Object.keys(map)
-    .map((entry) => ({ value: entry, count: map[entry].length }))
-    .sort((one, another) => another.count - one.count)
-    .map(({ value, count }) => `${value} ${count}`)
+  return Object.keys(result)
+    .map((item) => ({ word: item, counts: result[item] }))
+    .sort((one, another) => another.counts - one.counts)
+    .map(({ word, counts }) => `${word} ${counts}`)
     .join('\n')
-}
-
-const getListMap = (inputList) => {
-  const map = {}
-  for (const input of inputList) {
-    if (!map.hasOwnProperty(input.value)) {
-      map[input.value] = [input]
-    } else {
-      map[input.value].push(input)
-    }
-  }
-
-  return map
 }
 
 module.exports = {
