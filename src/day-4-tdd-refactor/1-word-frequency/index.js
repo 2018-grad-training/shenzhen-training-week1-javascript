9f90cf948a32d1e8642568b4da7213996ecf3c44
Linesh <linesh.simpcity@gmail.com>
2018-08-30 16:42:07 +0800

    [Linesh] refactor: extract methods

--- src/day-4-tdd-refactor/1-word-frequency/index.js
+++ src/day-4-tdd-refactor/1-word-frequency/index.js
@@ -3,22 +3,23 @@ const getResult = (words) => {
     return `${words} 1`
   }
 
-  const result = words
-    .split(' ')
-    .filter((input) => input)
-    .reduce(
-      (total, input) =>
-        Object.assign(total, {
-          [input]: total.hasOwnProperty(input) ? total[input] + 1 : 1,
-        }),
-      {}
-    )
+  const result = splitWords(words).reduce(
+    (total, input) =>
+      Object.assign(total, {
+        [input]: total.hasOwnProperty(input) ? total[input] + 1 : 1,
+      }),
+    {}
+  )
 
   return Object.keys(result)
     .map((item) => ({ word: item, counts: result[item] }))
     .sort((one, another) => another.counts - one.counts)
     .map(({ word, counts }) => `${word} ${counts}`)
     .join('\n')
+}
+
+function splitWords(words) {
+  return words.split(' ').filter((input) => input)
 }
 
 module.exports = {
