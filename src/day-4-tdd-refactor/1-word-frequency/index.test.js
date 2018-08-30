c6cd3a94da4b0925f3974b106a7dbc53cd2bcf4c
Linesh <linesh.simpcity@gmail.com>
2018-08-30 16:05:10 +0800

    [Linesh] feature: add word frequency home work

---
+++ src/day-4-tdd-refactor/1-word-frequency/index.test.js
@@ -0,0 +1,51 @@
+const { getResult } = require('./index')
+
+it('should get "the 1" when input "the"', () => {
+  const input = 'the'
+  const output = 'the 1'
+
+  validate_Input_words_process_to_expected_word(input, output)
+})
+
+it('should process two words', () => {
+  const input = 'the is'
+  const output = 'the 1\nis 1'
+
+  validate_Input_words_process_to_expected_word(input, output)
+})
+
+it('should process two words with special spaces', () => {
+  const input = 'the      is'
+  const output = 'the 1\nis 1'
+
+  validate_Input_words_process_to_expected_word(input, output)
+})
+
+it('should pracess two same words with sorted', () => {
+  const input = 'the the is'
+  const output = 'the 2\nis 1'
+
+  validate_Input_words_process_to_expected_word(input, output)
+})
+
+it('should process sorted with count descending', () => {
+  const input = 'the is is'
+  const output = 'is 2\nthe 1'
+
+  validate_Input_words_process_to_expected_word(input, output)
+})
+
+it('should process two same words with sorted', () => {
+  const input = 'the the is'
+  const output = 'the 2\nis 1'
+
+  validate_Input_words_process_to_expected_word(input, output)
+})
+
+// eslint-disable-next-line
+const validate_Input_words_process_to_expected_word = (input, output) => {
+  // When
+  const result = getResult(input)
+  // Then
+  expect(result).toBe(output)
+}
